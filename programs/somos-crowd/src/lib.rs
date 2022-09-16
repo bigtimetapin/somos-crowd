c8df63fc75dc85aa3459cb233fdd8eb460a5e800
alex <aabinaei@gmail.com>
2022-09-15 17:00:00 -0700

    create-metadata-ix

--- programs/somos-crowd/src/lib.rs
+++ programs/somos-crowd/src/lib.rs
@@ -1,12 +1,54 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Mint, Token};
 use mpl_token_metadata::state::{PREFIX, MAX_METADATA_LEN};
+use mpl_token_metadata::instruction::create_metadata_accounts_v3;
 
 declare_id!("HRLqhFshmXMXZmY1Fkz8jJTktMEkoxLssFDtydkW5xXa");
 
 #[program]
 pub mod somos_crowd {
     use super::*;
+
+    pub fn initialize_collection(
+        ctx: Context<InitializeCollection>,
+    ) -> Result<()> {
+        // build instruction
+        let ix = create_metadata_accounts_v3(
+            mpl_token_metadata::ID,
+            ctx.accounts.metadata.key(),
+            ctx.accounts.mint.key(),
+            ctx.accounts.authority.key(),
+            ctx.accounts.payer.key(),
+            ctx.accounts.authority.key(),
+            String::from("test-name"),
+            String::from("test-symbol"),
+            String::from("test-uri"),
+            None,
+            500,
+            false,
+            false,
+            None,
+            None,
+            None
+        );
+        // unwrap authority bump
+        let authority_bump = *ctx.bumps.get("authority").unwrap();
+        // build signer seeds
+        let seeds = &[
+            "authority".as_bytes(), &ctx.accounts.mint.key().to_bytes(),
+            &[authority_bump]
+        ];
+        let signer_seeds = &[&seeds[..]];
+        // invoke
+        anchor_lang::solana_program::program::invoke_signed(
+            &ix,
+            &[
+                ctx.accounts.metadata.to_account_info()
+            ],
+            signer_seeds
+        ).map_err(Into::into)
+    }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -15,7 +57,7 @@ pub mod somos_crowd {
 #[derive(Accounts)]
 pub struct InitializeCollection<'info> {
     #[account(init,
-    seeds = [b"authority"], bump,
+    seeds = [b"authority", mint.key().as_ref()], bump,
     payer = payer,
     space = Authority::SPACE
     )]
@@ -30,7 +72,7 @@ pub struct InitializeCollection<'info> {
     seeds = [
     PREFIX.as_bytes(),
     mpl_token_metadata::ID.as_ref(),
-    mint.to_account_info().key().as_ref()
+    mint.key().as_ref()
     ], bump,
     payer = payer,
     space = MAX_METADATA_LEN,
