f5b25867e0c818765bab69b8ecdd3b4fdc2e4d33
alex <aabinaei@gmail.com>
2022-10-01 14:49:32 -0700

    moved mint-new-copy to ix module

--- programs/somos-crowd/src/ix/mod.rs
+++ programs/somos-crowd/src/ix/mod.rs
@@ -1,3 +1,4 @@
 pub mod init_new_creator;
 pub mod create_nft;
 pub mod create_collection;
+pub mod mint_new_copy;
--- programs/somos-crowd/src/lib.rs
+++ programs/somos-crowd/src/lib.rs
@@ -1,15 +1,13 @@
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
-use anchor_spl::token::{mint_to, Mint, MintTo, Token, TokenAccount};
+use anchor_spl::token::{Mint, Token, TokenAccount};
 use mpl_token_metadata::state::{
-    PREFIX, EDITION, EDITION_MARKER_BIT_SIZE, CollectionDetails,
+    PREFIX, EDITION, EDITION_MARKER_BIT_SIZE,
 };
-use mpl_token_metadata::instruction::{
-    create_metadata_accounts_v3, create_master_edition_v3, sign_metadata,
-    mint_new_edition_from_master_edition_via_token, set_and_verify_sized_collection_item,
+use mpl_token_metadata::instruction::{set_and_verify_sized_collection_item,
 };
 use crate::pda::{authority::Authority, creator::Creator};
-use crate::ix::{init_new_creator, create_nft, create_collection};
+use crate::ix::{init_new_creator, create_nft, create_collection, mint_new_copy};
 
 pub mod pda;
 pub mod ix;
@@ -43,72 +41,7 @@ pub mod somos_crowd {
     }
 
     pub fn mint_new_copy(ctx: Context<MintNewCopy>) -> Result<()> {
-        let increment = ctx.accounts.authority.num_minted + 1;
-        // unwrap authority bump
-        let authority_bump = *ctx.bumps.get(pda::authority::SEED).unwrap();
-        // build signer seeds
-        let seeds = &[
-            pda::authority::SEED.as_bytes(), &ctx.accounts.mint.key().to_bytes(),
-            &[authority_bump]
-        ];
-        let signer_seeds = &[&seeds[..]];
-        // build ata new-edition instruction
-        let ata_cpi_accounts = MintTo {
-            mint: ctx.accounts.new_mint.to_account_info(),
-            to: ctx.accounts.new_edition_ata.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(),
-        };
-        let ata_cpi_context = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            ata_cpi_accounts,
-        );
-        // build new-edition instruction
-        let ix_new_edition = mint_new_edition_from_master_edition_via_token(
-            ctx.accounts.metadata_program.key(),
-            ctx.accounts.new_metadata.key(),
-            ctx.accounts.new_edition.key(),
-            ctx.accounts.master_edition.key(),
-            ctx.accounts.new_mint.key(),
-            ctx.accounts.authority.key(),
-            ctx.accounts.payer.key(),
-            ctx.accounts.authority.key(),
-            ctx.accounts.master_edition_ata.key(),
-            ctx.accounts.authority.key(),
-            ctx.accounts.metadata.key(),
-            ctx.accounts.mint.key(),
-            increment,
-        );
-        // invoke mint-to ata new-edition
-        mint_to(
-            ata_cpi_context.with_signer(
-                signer_seeds
-            ),
-            1,
-        )?;
-        // invoke new edition
-        anchor_lang::solana_program::program::invoke_signed(
-            &ix_new_edition,
-            &[
-                ctx.accounts.new_metadata.to_account_info(),
-                ctx.accounts.new_edition.to_account_info(),
-                ctx.accounts.master_edition.to_account_info(),
-                ctx.accounts.new_mint.to_account_info(),
-                ctx.accounts.new_edition_mark.to_account_info(),
-                ctx.accounts.authority.to_account_info(),
-                ctx.accounts.payer.to_account_info(),
-                ctx.accounts.authority.to_account_info(),
-                ctx.accounts.master_edition_ata.to_account_info(),
-                ctx.accounts.authority.to_account_info(),
-                ctx.accounts.metadata.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-                ctx.accounts.rent.to_account_info(),
-            ],
-            signer_seeds,
-        )?;
-        // increment
-        let authority = &mut ctx.accounts.authority;
-        authority.num_minted = increment;
-        Ok(())
+        mint_new_copy::ix(ctx)
     }
 
     pub fn add_new_copy_to_collection(ctx: Context<AddNewCopyToCollection>) -> Result<()> {
